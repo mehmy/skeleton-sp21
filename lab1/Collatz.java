47133c393102f4c07b62ad6628a81bae7fe53cde
mehmy <m.yalvac98@gmail.com>
2025-05-31 12:28:53 +0200

    collatz problem fixed with the correct solution

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,15 +5,17 @@ package lab1;
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * if n is an even number, the number will get divided by 2
+     * if n is an uneven number, the number will be multiplied by 3 and added by 1
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0){
+            return n / 2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
+
     }
 
     public static void main(String[] args) {
