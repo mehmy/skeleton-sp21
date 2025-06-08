ae36bea76c21638e166238c41367a1bb1c89a810
mehmy <m.yalvac98@gmail.com>
2025-06-08 13:31:41 +0200

    fixed early end recursion

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -70,8 +70,8 @@ public class DebugExercise2 {
      * */
     public static int sumOfElementwiseMaxes(int[] a, int[] b) {
         int[] maxes = arrayMax(a, b);
-        int sumofMaxes = arraySum(maxes);
-        return sumofMaxes;
+        assert maxes != null;
+        return arraySum(maxes);
     }
 
 
